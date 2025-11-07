70f65f27393019ed3ddfb88ce542b24d79d66a28
Mahima Chhagani <chhaganimahima@gmail.com>
2025-11-06 19:59:27 -0500

    resolving Q6,Q8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,8 +43,8 @@ mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processe
 cp ./data/raw/*server*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/*user*.log ./data/processed/server_logs
-cp ./data/raw/*event*.log ./data/processed/server_logs
+cp ./data/raw/*user*.log ./data/processed/user_logs
+cp ./data/raw/*event*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr* 
@@ -52,8 +52,7 @@ rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt
-cd ./data/processed
-ls > ./data/inventory.txt
+ls  ./data/inventory.txt > ./data/processed
 
 ###########################################
 
