a36359f724fb012b0a1adda99699ff328ea7dd2e
Mahima Chhagani <chhaganimahima@gmail.com>
2025-11-06 20:08:49 -0500

    resolving Q8 attempt2

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -52,7 +52,7 @@ rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt
-ls  ./data/inventory.txt > ./data/processed/
+ls ./data/processed/* > ./data/inventory.txt
 
 ###########################################
 
